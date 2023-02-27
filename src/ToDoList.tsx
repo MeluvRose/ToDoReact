60277d0e4b737387f8277462765e7cdec4a92467
meluvrose <tanggu3910@gmail.com>
2023-02-28 01:46:30 +0900

    #6.6 Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,45 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
+/*
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [error, SetError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    SetError("");
     setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) SetError("It's Too Short...");
+    else console.log(toDo);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {error !== " " ? error : null}
+      </form>
+    </div>
+  );
+}
+*/
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  // console.log(register("toDo"));
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("Username")} placeholder="Username" />
+        <input {...register("Password")} placeholder="Password" />
+        <input {...register("Password1")} placeholder="Password1" />
         <button>Add</button>
       </form>
     </div>
