956d493e54de09fe89b355d95e3fb403c5f439c3
meluvrose <tanggu3910@gmail.com>
2023-02-27 23:30:53 +0900

    #6.5 To Do Setup

---
+++ src/theme.tsx
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#130f40",
+  textColor: "#535c68",
+  accentColor: "#f9ca24",
+  cardBgColor: "transparant",
+};
