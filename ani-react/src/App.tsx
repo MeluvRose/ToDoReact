1c8f631b2bb077016f1233012c4f3023e409d6eb
meluvrose <tanggu3910@gmail.com>
2023-03-03 21:56:26 +0900

    #8.6 Gestures part 1

--- ani-react/src/App.tsx
+++ ani-react/src/App.tsx
@@ -12,63 +12,32 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  background-color: white;
-  border-radius: 50%;
-  box-shadow: 0 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVarients = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { borderRadius: `50%`, scale: 1 },
+  drag: {
+    backgroundColor: "rgba(46, 204, 113, 1.0)",
     transition: {
-      type: "spring",
-      duration: 3,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.25,
+      duration: 10,
     },
-  },
-};
-
-const circleVarients = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-    // transition: {
-    //   delay: 0.5,
-    // },
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVarients} initial="start" animate="end">
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-        <Circle variants={circleVarients} />
-      </Box>
+      <Box
+        drag
+        variants={boxVarients}
+        whileDrag="drag"
+        whileHover="hover"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
