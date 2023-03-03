66df5f7c7fbf29aaca1f43c0937fb5062cfc40be
meluvrose <tanggu3910@gmail.com>
2023-03-03 16:50:15 +0900

    #7.14 Task Objects

--- trello-clone/src/Components/Board.tsx
+++ trello-clone/src/Components/Board.tsx
@@ -1,8 +1,9 @@
 import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DraggableCard";
 import { useRecoilState } from "recoil";
-import { toDoState } from "../states";
+import { IToDo, toDoState } from "../states";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const BoardStructure = styled.div`
   width: 300px;
@@ -40,14 +41,38 @@ const Area = styled.div<IAreaProps>`
 `;
 
 interface IBoardProps {
-  toDos: string[];
+  toDos: IToDo[];
   boardId: string;
 }
 
+interface IForm {
+  toDo: string;
+}
+
+const Form = styled.form`
+  width: 100%;
+  input {
+    width: 100%;
+  }
+`;
+
 function Board({ toDos, boardId }: IBoardProps) {
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onVaild = (toDo: IForm) => {
+    console.log(toDo);
+    setValue("toDo", "");
+  };
+
   return (
     <BoardStructure>
       <Title>{boardId}</Title>
+      <Form onSubmit={handleSubmit(onVaild)}>
+        <input
+          {...register("toDo", { required: true })}
+          type="text"
+          placeholder={`Add task on ${boardId}`}
+        />
+      </Form>
       <Droppable droppableId={boardId}>
         {(magic, snapshot) => (
           <Area
@@ -57,7 +82,12 @@ function Board({ toDos, boardId }: IBoardProps) {
             {...magic.droppableProps}
           >
             {toDos.map((toDo, index) => (
-              <DraggableCard key={toDo} toDo={toDo} index={index} />
+              <DraggableCard
+                key={toDo.id}
+                index={index}
+                toDoText={toDo.text}
+                toDoId={toDo.id}
+              />
             ))}
             {magic.placeholder}
           </Area>
