cffff3c6af7d35ff53b4ba22b969e8976e8f7ec0
meluvrose <tanggu3910@gmail.com>
2023-03-05 22:25:00 +0900

    #8.14 You Need to Watch this

--- ani-react/src/App.tsx
+++ ani-react/src/App.tsx
@@ -12,8 +12,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
@@ -27,8 +26,6 @@ const Box = styled(motion.div)`
   align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   font-size: 18px;
-  position: absolute;
-  top: 100px;
 `;
 
 const boxVariants = {
@@ -51,35 +48,36 @@ const boxVariants = {
   }),
 };
 
+const Circle = styled(motion.div)`
+  background-color: #a4e2f6;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+`;
+
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence exitBeforeEnter custom={back}>
-        {
-          <Box
-            key={visible}
-            variants={boxVariants}
-            custom={back}
-            initial="entry"
-            animate="center"
-            exit="exit"
-          >
-            {visible}
-          </Box>
-        }
-      </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      {/* <Box
+        style={{
+          justifyContent: clicked ? "center" : "flex-start",
+          alignItems: clicked ? "center" : "flex-start",
+        }}
+      >
+        <Circle layout />
+      </Box> */}
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: "25px" }} />
+        ) : null}
+      </Box>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
