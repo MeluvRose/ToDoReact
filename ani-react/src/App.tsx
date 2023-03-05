5494bebd5f263f075506047126ee77e1514ed85a
meluvrose <tanggu3910@gmail.com>
2023-03-05 22:42:15 +0900

    #8.15 Final Project part 1

--- ani-react/src/App.tsx
+++ ani-react/src/App.tsx
@@ -16,68 +16,56 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  font-size: 18px;
-`;
-
-const boxVariants = {
-  entry: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-};
-
-const Circle = styled(motion.div)`
-  background-color: #a4e2f6;
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
 `;
 
 function App() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const toggle = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      {/* <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
-      </Box> */}
-      <Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="Hello"></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Grid>
+      <AnimatePresence>
         {clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: "25px" }} />
+          <Overlay
+            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.8)" }}
+            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+          >
+            <Box layoutId="Hello" style={{ width: 320, height: 240 }} />
+          </Overlay>
         ) : null}
-      </Box>
-      <Box>
-        {!clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
-        ) : null}
-      </Box>
+      </AnimatePresence>
     </Wrapper>
   );
 }
